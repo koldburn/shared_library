4f0e7d6f5f3b80259032287017a07d27c69890de
koldburn <68352536+koldburn@users.noreply.github.com>
2020-09-22 21:09:04 +0000

    Create test_file.groovy

---
+++ vars/test_file.groovy
@@ -0,0 +1,12 @@
+def call(){
+  def name = 'batman'
+  def city = 'Gotham'
+  def villain = 'joker'
+  
+  if (name == 'batman){
+    sh 'echo this is batman'
+  } else if (city=='Gotham'){
+      sh 'echo this is Gotham city'
+    } else(villain=='joker'){
+        sh 'echo this is joker'
+ }
